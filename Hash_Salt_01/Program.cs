51cefde32e9824be99e4d4f3c2c32511157f837a
Phil Anderson <philanderson888@hotmail.com>
2020-02-07 21:30:03 +0000

    adding console-core.sln

---
+++ Hash_Salt_01/Program.cs
@@ -0,0 +1,51 @@
+﻿#region using Cryptography and LINQ
+using System;
+using System.Security.Cryptography;
+using System.Linq;
+#endregion
+namespace Hash_Salt_01
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            #region Generate A New Hash And Just Read The Salt
+            var salt = new Hash().Salt;
+            #endregion
+            #region Print Out The Salt Byte Array
+            Console.WriteLine("Salt as hex");  
+            Console.WriteLine(BitConverter.ToString(salt));
+            Console.WriteLine("Salt as decimal bytes");
+            foreach(var item in salt){
+                Console.Write(item + "   ");
+            }
+            #endregion
+        }
+    }
+    #region Class Hash - Generates A Salt
+    class Hash
+    {
+        private static readonly RandomNumberGenerator rng = RandomNumberGenerator.Create();
+        public static readonly int DefaultSaltSize = 8; // 64-bit salt
+        public readonly byte[] Salt;
+        public readonly byte[] Passhash;
+
+        public Hash()
+        {
+            Salt = GenerateSalt(DefaultSaltSize);
+        }
+
+        private static byte[] GenerateSalt(int saltSize)
+        {
+            // This generates salt.
+            // Rephrasing Schneier:
+            // "salt" is a random string of bytes that is
+            // combined with password bytes before being
+            // operated by the one-way function.
+            byte[] salt = new byte[saltSize];
+            rng.GetBytes(salt);
+            return salt;
+        }
+    }
+    #endregion
+}
---
+++ Hash_Salt_02/Program.cs
@@ -0,0 +1,39 @@
+﻿#region Using Libraries
+using System;
+using System.Security.Cryptography;
+#endregion
+namespace Hash_Salt_02
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            #region Declare Variables salt
+            byte[] salt;  // this will hold the salt
+            #endregion
+            #region Generate And Print New Salt
+            new RNGCryptoServiceProvider().GetBytes(salt=new byte[16]);
+            Console.WriteLine("New Salt Is Generated Which Is");
+            Console.WriteLine(BitConverter.ToString(salt));
+            #endregion
+            #region Get Initial Password
+            Console.WriteLine("Please Enter Password");
+            string initialPassword = Console.ReadLine();
+            #endregion
+            #region Join Salt To Password And Generate Hash With PBKDF2
+            var pbkdf2 = new Rfc2898DeriveBytes(initialPassword, salt, 10000);
+            // getbytes places a string into a byte array
+            byte[] hash = pbkdf2.GetBytes(20);
+            byte[] hashbytes = new byte[36];
+            // Array.Copy(array1,startindex,array2,startdestinationindex,lengthtocopy)
+            Array.Copy(salt, 0, hashbytes, 0, 16);  // copy 16 bytes from salt to hashbytes
+            Array.Copy(hash, 0, hashbytes, 16, 20); // copy 20 bytes from hash to hashbytes
+            string savedPasswordHash = Convert.ToBase64String(hashbytes);
+            Console.WriteLine("Hash Bytes Array Holds");       
+            Console.Write(BitConverter.ToString(hashbytes));
+            Console.WriteLine();
+            Console.WriteLine($"saved password hash is {savedPasswordHash} after converting to Base64 as a string");
+            #endregion
+        }
+    }
+}
