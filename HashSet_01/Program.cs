1c59748f15973701b9364ed801331e3e2b3c6d55
Phil Anderson <philanderson888@hotmail.com>
2020-02-08 13:34:57 +0000

    Adding Entity Framework Core Database Generation Program

---
+++ HashSet_01/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace HashSet_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var hashset = new HashSet<int>();
+            hashset.Add(10);
+            hashset.Add(20);
+            hashset.Add(10); // duplicate value overwritten but no exception
+            foreach (var item in hashset) {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
