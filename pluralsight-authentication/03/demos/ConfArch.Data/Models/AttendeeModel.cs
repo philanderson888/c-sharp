2df89247191a87e9cc2657f9f2df37916707206e
philanderson888 <philanderson888@hotmail.com>
2020-05-13 21:34:03 +0100

    Pluralsight Authentication Removing Extra Project

--- pluralsight-authentication/03/demos/ConfArch.Data/Models/AttendeeModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ConfArch.Data.Models
-{
-    public class AttendeeModel
-    {
-        public int Id { get; set; }
-        public int ConferenceId { get; set; }
-        public string Name { get; set; }
-    }
-}
