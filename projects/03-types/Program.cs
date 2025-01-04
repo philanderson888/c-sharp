d4974a9b9c06c5e67298b15e24a94920eb3bb116
philanderson888 <philanderson888@hotmail.com>
2025-01-04 08:43:52 +0000

    add types

---
+++ projects/03-types/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.Runtime.ExceptionServices;
+
+namespace types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = default;
+            WriteLine($"x is {x}");
+        }
+    }
+}
