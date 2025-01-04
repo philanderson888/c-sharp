191dfece98a99ee736fcb3690f9cf1c85fb496d2
philanderson888 <philanderson888@hotmail.com>
2025-01-04 08:43:41 +0000

    add dummy project to run others

---
+++ projects/00-run/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security;
+using System.Runtime.ExceptionServices;
+
+namespace run
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
